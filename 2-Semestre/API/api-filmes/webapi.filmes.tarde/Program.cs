54f1d1a9dc4d7c93efdb22869afaf092a1d82723
Murilo Souza ALmeida <murilo.souza.senai@gmail.com>
2023-09-04 15:18:47 -0300

    Geração do token

--- 2-Semestre/API/api-filmes/webapi.filmes.tarde/Program.cs
+++ 2-Semestre/API/api-filmes/webapi.filmes.tarde/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System.Reflection;
 
@@ -14,7 +15,32 @@ builder.Services.AddAuthentication(options =>
 })
 
 //Define os parâmetros de validaçào do token (continuação acima)
-.AddJwtBearer(options => {  }); //paramos aqui - continuar na segunda
+.AddJwtBearer("JwtBearer",options => 
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        //Valida quem está solicitando
+        ValidateIssuer = true,
+
+        //Valida quem está recebendo
+        ValidateAudience = true,
+
+        //Define se o tempo de expiração do token será validado
+        ValidateLifetime = true,
+
+        //Forma de criptografia e a validação da chave de autenticação
+        IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("filmes-chave-autenticacao-webapi-dev")),
+
+        //Valida o tempo de expiração do token
+        ClockSkew = TimeSpan.FromMinutes(5),
+
+        //De onde está vindo (qual o issure)
+        ValidIssuer = "webapi.filmes.tarde",
+
+        //Para onde está indo (qual o audiece)
+        ValidAudience = "webapi.filmes.tarde"
+    };
+});
 
 //Adiciona o gerador do Swagger à coleção de serviços
 builder.Services.AddSwaggerGen(options =>
@@ -74,6 +100,12 @@ app.UseSwaggerUI(options =>
     //Defina a propriedade RoutePrefix como uma cadeia de caracteres vazia
     options.RoutePrefix = string.Empty;
 });
+
+//Use a autenticação definida pelo token
+app.UseAuthentication();
+
+//Use a autorização definidas no token
+app.UseAuthorization();
 
 //Mapear os controllers
 app.MapControllers();
