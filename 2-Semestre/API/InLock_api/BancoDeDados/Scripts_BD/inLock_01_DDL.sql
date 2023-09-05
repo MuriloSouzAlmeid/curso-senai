009ec6b301d64f94cb9363b8a17c171d155badee
Murilo Souza ALmeida <murilo.souza.senai@gmail.com>
2023-09-05 16:50:26 -0300

    Início do projeto InLock Games

---
+++ 2-Semestre/API/InLock_api/BancoDeDados/Scripts_BD/inLock_01_DDL.sql
@@ -0,0 +1,39 @@
+CREATE  DATABASE InLock_games_tarde;
+GO
+
+USE InLock_games_tarde;
+GO
+
+CREATE TABLE Estudio
+(
+	IdEstudio INT PRIMARY KEY IDENTITY
+	,Nome VARCHAR(100) NOT NULL
+);
+GO
+
+CREATE TABLE Jogo
+(
+	IdJogo INT PRIMARY KEY IDENTITY
+	,IdEstudio INT FOREIGN KEY REFERENCES Estudio(IdEstudio)
+	,Nome VARCHAR(100) NOT NULL
+	,Descricao VARCHAR(100) NOT NULL
+	,DataLancamento DATE NOT NULL
+	,Valor SMALLMONEY NOT NULL
+);
+GO
+
+CREATE TABLE TiposUsuario
+(
+	IdTipoUsuario INT PRIMARY KEY IDENTITY
+	,Titulo VARCHAR(100) NOT NULL
+);
+GO
+
+CREATE TABLE Usuario
+(
+	IdUsuario INT PRIMARY KEY IDENTITY
+	,IdTipoUsuario INT FOREIGN KEY REFERENCES TiposUsuario(IdTipoUsuario)
+	,Email VARCHAR(100) NOT NULL UNIQUE
+	,Senha VARCHAR (100) NOT NULL
+);
+GO
