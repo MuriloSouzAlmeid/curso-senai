6f99f5ed97dd84e9c126dc6ef5a2487167460949
Murilo Souza Almeida <murilo.souza.senai@gmail.com>
2023-11-09 17:25:58 -0300

    início no uso de useEffects e axios para acessar api no React.js

--- 2-Semestre/React/Event+/eventplus/src/pages/HomePage/HomePage.jsx
+++ 2-Semestre/React/Event+/eventplus/src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "./HomePage.css";
 
 //import dos componentes
@@ -11,36 +12,68 @@ import NextEvent from "../../components/NextEvent/NextEvent";
 import Container from "../../components/Container/Container";
 
 const HomePage = () => {
+  //chamar a api na hora que carregar a página
+  //usamos o useEffect, ele sempre roda uma primeira vez mesmo não tendo alterado a variável
+  useEffect(
+    //função que será executada quando o useEffect for chamado (irá chamar a api)
+    async function getProximosEventos () {
+      try{
+        const promisse = await axios.get('http://localhost:5000/api/Evento/ListarProximos');
+
+        setNextEvents(promisse.data); //o data acessa os dados no objeto json
+      }catch(error){
+        console.log(error);
+      }
+      getProximosEventos();
+    }
+    , 
+    [] //array de dependências para indicar quando o comando será executado (vazia roda uma vez só quando a página for carregada)
+    //podemos colocar várias variáveis como dependência e quando qualquer uma delas for alterada o useEffect é executado
+  );
+
+  //fake mock - api mocada
+  const [nextEvents, setNextEvents] = useState([
+    {
+      id: 1,
+      title: "Evento A",
+      date: "12/11/2023",
+      description: "Evento legal sobre assunto A",
+    },
+    {
+      id: 2,
+      title: "Evento B",
+      date: "25/12/2023",
+      description: "Evento legal sobre assunto B",
+    },
+    {
+      id: 3,
+      title: "Evento C",
+      date: "01/01/2024",
+      description: "Evento legal sobre assunto C",
+    },
+  ]);
+
   return (
     //quando tem componentes dentro de um outro componente usamos um elemento duplo (com abertura e fechamento)
     <MainContent>
       <Banner />
 
       {/* PRÓXIMOS EVENTOS */}
-
       <section className="proximos-eventos">
         <Container>
           <Titulo titleText={"Próximos Eventos"} />
 
           <div className="events-box">
-            <NextEvent
-              title={"Título Card"}
-              description={"Descrição"}
-              eventDate={"12/11/2005"}
-              idEvento={12345}
-            />
-            <NextEvent
-              title={"Título Card"}
-              description={"Descrição"}
-              eventDate={"12/11/2005"}
-              idEvento={54321}
-            />
-            <NextEvent
-              title={"Título Card"}
-              description={"Descrição ighljgkhjvhjvh"}
-              eventDate={"12/11/2005"}
-              idEvento={67890}
-            />
+            {nextEvents.map((e) => {
+              return (
+                <NextEvent
+                  title={e.title}
+                  description={e.description}
+                  eventDate={e.date}
+                  idEvento={e.id}
+                />
+              );
+            })}
           </div>
         </Container>
       </section>
--- 2-Semestre/React/Event+/eventplus/src/pages/TestePage/TestePage.jsx
+++ 2-Semestre/React/Event+/eventplus/src/pages/TestePage/TestePage.jsx
@@ -1,50 +1,16 @@
-import React, { useState } from "react";
-import Input from "../../components/Input/Input";
-import Button from "../../components/Button/Button";
+import React from 'react';
+import { useState } from 'react';
 
 const TestePage = () => {
-  const [total, setTotal] = useState();
-  const [n1, setN1] = useState();
-  const [n2, setN2] = useState();
-
-  function handleSomar(e) {
-    //passa o event automaticamente
-    e.preventDefault();
-    setTotal(parseFloat(n1) + parseFloat(n2));
-  }
+  const [count, setCount] =useState(10);
+  // const [calculadora, setCalculadora] = useState(10);
 
   return (
-    <>
-      <h1>Página de Testes</h1>
-      <h2>Calculadora de Soma</h2>
-      <form onSubmit={handleSomar}>
-        {/* ao criar o elemento é como se eu estivesse instanciando um objeto da classe do elemento */}
-        <Input
-          // é como se fossem os parâmetros para construir o objeto
-          tipo="number"
-          nome="numero1"
-          id="numero1"
-          dicaCampo="Digite o 1º número"
-          valor={n1}
-          //função que altera o valor do campo passando por herança
-          alteraValor={setN1}
-        />
-        <br />
-        <Input
-          tipo="number"
-          nome="numero2"
-          id="numero2"
-          dicaCampo="Digite o 2º número"
-          valor={n2}
-          alteraValor={setN2}
-        />
-        <br />
-        <Button tipo="submit" texto="Somar" />
-        <p>
-          Resultado: <strong>{total}</strong>
-        </p>
-      </form>
-    </>
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount((c) => c + 1)}>+</button>
+      {/* <p>Calculation: {calculadora}</p>     */}
+    </div>
   );
 };
 
