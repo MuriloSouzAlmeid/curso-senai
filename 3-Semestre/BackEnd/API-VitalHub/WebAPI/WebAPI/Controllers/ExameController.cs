4935b0410fde747ef63bc9217a46d3344e171aed
Murilo Souza Almeida <murilo.souza.senai@gmail.com>
2024-04-19 17:00:37 -0300

    Inclusão de serviços OCR na api vitalHUb

--- 3-Semestre/BackEnd/API-VitalHub/WebAPI/WebAPI/Controllers/ExameController.cs
+++ 3-Semestre/BackEnd/API-VitalHub/WebAPI/WebAPI/Controllers/ExameController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebAPI.Domains;
+using WebAPI.Interfaces;
+using WebAPI.Utils.OCR;
+using WebAPI.ViewModels;
 
 namespace WebAPI.Controllers
 {
@@ -7,6 +11,60 @@ namespace WebAPI.Controllers
     [ApiController]
     public class ExameController : ControllerBase
     {
-        //aqui vai a lógica da OCR
+        private readonly IExameRepository _exameRepository;
+        private readonly OCRService _ocrService;
+        public ExameController(IExameRepository exameRepository, OCRService ocrService) 
+        {
+            _exameRepository = exameRepository;
+            _ocrService = ocrService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromForm] ExameViewModel exameViewModel)
+        {
+            try
+            {
+                if (exameViewModel.Imagem == null || exameViewModel == null)
+                {
+                    return BadRequest("Nenhuma Imagem Fornecida");
+                }
+
+                using (var stream = exameViewModel.Imagem.OpenReadStream())
+                {
+                    var result = await _ocrService.RecognizeTextAsync(stream);
+
+                    exameViewModel.Descricao = result;
+
+                    Exame exame = new Exame()
+                    {
+                        Descricao = exameViewModel.Descricao,
+                        ConsultaId = exameViewModel.ConsultaId
+                    };
+
+                    _exameRepository.Cadastrar(exame);
+
+                    return Ok(exame);
+                }
+            }
+            catch (Exception erro)
+            {
+                return BadRequest(erro.Message);
+            }
+        }
+
+        [HttpGet("BuscarPorId")]
+        public IActionResult GetByIdConsult(Guid id)
+        {
+            try
+            {
+                List<Exame> lista = _exameRepository.BuscarPorIdConsulta(id);
+
+                return Ok(lista);
+            }
+            catch (Exception erro)
+            {
+                return BadRequest(erro.Message);
+            }
+        }
     }
 }
---
+++ 3-Semestre/BackEnd/API-VitalHub/WebAPI/WebAPI/Utils/OCR/OCRService.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+
+namespace WebAPI.Utils.OCR
+{
+    public class OCRService
+    {
+        private readonly string _subscriptKey = "9bf643b46b6d47bdac0b014c46c6f649";
+
+        private readonly string _endPoint = "https://cvviatlhubmurilosouza.cognitiveservices.azure.com/";
+
+        public async Task<string> RecognizeTextAsync(Stream imageStream)
+        {
+            try
+            {
+                var client = new ComputerVisionClient(new ApiKeyServiceClientCredentials(_subscriptKey))
+                {
+                     Endpoint = _endPoint
+                };
+
+                var ocrResult = await client.RecognizePrintedTextInStreamAsync(true, imageStream);
+
+                return ProcessRecognitionResult(ocrResult);
+            }
+            catch (Exception ex)
+            {
+                return "Erro ao reconhecer o texto";
+            }           
+        }
+
+        private static string ProcessRecognitionResult(OcrResult result)
+        {
+            try
+            {
+                string recognizedText = "";
+
+                foreach (var region in result.Regions)
+                {
+                    foreach (var line in region.Lines)
+                    {
+                        foreach (var word in line.Words)
+                        {
+                            recognizedText += word + " ";
+                        }
+                        recognizedText += "\n";
+                    }
+                }
+
+                return recognizedText;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
