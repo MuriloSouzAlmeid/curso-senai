53abc81ccfe4998a49376446d6ee830adb99cbc8
Murilo Souza Almeida <murilo.souza.senai@gmail.com>
2024-08-07 17:27:37 -0300

    início da sprint de revisão

---
+++ 4-semestre/RevisaoCurso/LogicaDeProgramacao/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Informe sua nota");
+float nota = float.Parse(Console.ReadLine());
+
+if(nota >= 5){
+    Console.WriteLine("Aprovado");
+}else{
+    Console.WriteLine("Reprovado");
+}
