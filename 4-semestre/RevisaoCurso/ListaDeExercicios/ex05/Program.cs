bd65b75d140d9db8c5472c9af03b7ad50a645c0a
Murilo Souza Almeida <murilo.souza.senai@gmail.com>
2024-08-15 17:23:03 -0300

    início do projeto de revisão Hospedagens.com

--- 4-semestre/RevisaoCurso/ListaDeExercicios/ex05/Program.cs
+++ 4-semestre/RevisaoCurso/ListaDeExercicios/ex05/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
 string[] alfabetoArray = "a b c d e f g h i j k l m n o p q r s t u v w x y z".Split(" ");
 
 Console.WriteLine($"Digite um texto a ser analizado");
