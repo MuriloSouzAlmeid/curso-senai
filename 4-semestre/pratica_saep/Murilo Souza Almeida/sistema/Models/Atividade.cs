d23ad4a9848a552a8237ab2bdc876ea5f55421be
Murilo Souza Almeida <murilo.souza.senai@gmail.com>
2024-10-11 17:16:50 -0300

    Início da simuação para a prova prática do SAEP

---
+++ 4-semestre/pratica_saep/Murilo Souza Almeida/sistema/Models/Atividade.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace sistema.Models;
+
+public partial class Atividade
+{
+    public int AtividadeId { get; set; }
+
+    public int? TurmaId { get; set; }
+
+    public string Descricao { get; set; } = null!;
+
+    public virtual Turma? Turma { get; set; }
+}
